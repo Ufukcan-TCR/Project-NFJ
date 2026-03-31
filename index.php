fb6920a124366af78b66ab204126f4a28ab94275
Ufukcan-TCR <9030526@student.zadkine.nl>
2026-03-31 15:02:09 +0200

    Home pagina skelet

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ouderavond</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="main">
+    <div class="container">
+        <div class="begin">
+            <img src="pictures/bear.png" alt="Legends of gaming logo" width="150">
+            <h1>Legends Of <br> Gaming</h1>
+        </div>
+        <div class="middle-group">
+            <div class="selected">Home</div>
+            <div class="item"><a href="overMij.html">Over mij</a></div>
+            <div class="item"><a href="eduArte.html">EduArte</a></div>
+            <div class="item"><a href="Quiz.html">Quiz</a></div>
+        </div>
+
+    <div class="end"> Log In</div>
+    </div>
+        
+
+
+    <footer>©2025 Ufukcan Kaynar</footer>
+</body>
+</html>
